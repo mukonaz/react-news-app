d3709e6cbfd2e0dade53c4ddb2f09b39f48af687
Mukona <iammthombeni@gmail.com>
2024-08-02 11:53:10 +0200

    add a login form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewsApp from './NewsApp';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <NewsApp />
+    </div>
+  );
+};
+
+export default App;
